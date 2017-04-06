b28fb42562e91c515abd68fb3f2282f73c31e19e
Andres Lopez Urena <b43885@ecci.ucr.ac.cr>
2017-04-06 13:02:09 -0600

    feat: implemented sample space

--- graficarSenal.py
+++ graficarSenal.py
@@ -3,11 +3,12 @@ from matplotlib.pyplot import figure, plot, show
 #signal function is as follows 
 #s(t) = 1/4 + sum((-1 + (-1)^k)/(pi*k)^2 cos(2pi*k*f_0*t + pi))
 
-#cosntants
+#constants
 a_0 = 1/4
 pi = np.pi
+samples = 100 #Samples to generate in interval for numpy linespace
 def getAsubks(kMax):
-    return [(-1 + (-1)**k)/(pi*k)**2 for k in xrange(1, kMax)]
+    return [abs((-1 + (-1)**k)/(pi*k)**2) for k in xrange(1, kMax)]
 
 def getCosine(theta, k, t, f_0):
     return np.cos(2*pi*k*f_0*t + theta)
@@ -15,9 +16,12 @@ def getCosine(theta, k, t, f_0):
 def listToNPArray(list):
     return np.array(list)
 
-def calculateFourierSeriesCoefs(kMax, theta, t, T):
+def getIntervalSamples(Tmin, Tmax):
+    return np.linspace(Tmin, Tmax, samples)
+def calculateFourierSeriesCoefs(kMax, theta, T, Tmin, Tmax):
     f_0 = 1/T;
     A = listToNPArray(getAsubks(kMax))
+    t_n = getIntervalSamples(Tmin, Tmax)
     cosines = [getCosine(theta, k, t, f_0) for k in xrange(1, kMax)]
     cosines = listToNPArray(cosines)
     return A, cosines
